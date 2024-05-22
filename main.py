c3c9160e9289eb4e2fd2004f04fd19b9dfaf764c
hagd <hagd0520@gmail.com>
2024-05-22 11:29:20 +0900

    4-1 type hint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from typing import Dict, List
+from fastapi import FastAPI, Query
 
 
 app = FastAPI()
@@ -9,10 +10,26 @@ def read_root():
     return {"message": "Hello, World!"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id):
-    return {"item_id": item_id}
+# @app.get("/items")
+# def read_items(skip = 0, limit = 10):
+#     return {"skip": skip, "limit": limit}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int):
+#     return {"item_id": item_id}
+
 
 @app.get("/items")
-def read_items(skip = 0, limit = 10):
-    return {"skip": skip, "limit": limit}
+async def read_items(q: List[int] = Query([])):
+    return {"q": q}
+
+
+@app.post("/create-item/")
+async def create_item(item: Dict[str, int]):
+    return item
+
+
+@app.get("/getdata")
+def read_items(data: str = "funcoding"):
+    return {"data": data}
