69af06f4d2806e206d90a816e19f95f39efbca8f
hagd <hagd0520@gmail.com>
2024-05-22 10:18:26 +0900

    3-1 path, params

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ def read_root():
     return {"message": "Hello, World!"}
 
 
-app.get("/items/{item_id}")
+@app.get("/items/{item_id}")
+def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.get("/items")
+def read_items(skip = 0, limit = 10):
+    return {"skip": skip, "limit": limit}
