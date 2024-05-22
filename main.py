7edaa128220f4937988b82ee9d0de897e006f7c6
hagd <hagd0520@gmail.com>
2024-05-22 20:00:13 +0900

    9-2 Jinja2 basic grammar 1

--- main.py
+++ main.py
@@ -3,8 +3,29 @@ from starlette.templating import Jinja2Templates
 
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates", auto_reload=True)
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/safe")
+def read_root_safe(request: Request):
+    my_variable_with_html = "<h1>Hello, FastAPI!</h1>"
+    return templates.TemplateResponse(
+        "index_with_safe.html",
+        {"request": request, "my_variable_with_html": my_variable_with_html}
+    )
+    
 
 @app.get("/")
 def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "username": "John Doe"})
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "my_variable": "Hello, FastAPI!"}
+    )
+    
+    
+@app.get("/greet")
+def greeting(request: Request, time_of_day: str):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "time_of_day": time_of_day}
+    )
