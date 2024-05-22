414d08d3763580bff4b8fd276bf74b86ce19b931
hagd <hagd0520@gmail.com>
2024-05-22 16:47:19 +0900

    8-2-1 Query variables

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 
-# @app.get("/users")
-# def read_users(q: str = Query(None, max_length=50)):
-#     return {"q": q}
+@app.get("/info")
+def read_info(info: str = Query(None, description="정보를 입력해 주세요.")):
+    return {"info": info}
 
 
 @app.get("/items")
-def read_items(internal_query: str = Query(None, alias="search")):
-    return {"query_handled": internal_query}
-
-
-@app.get("/users")
-def read_users(q: str = Query(None, deprecated=True)):
-    # 특정 쿼리 파라미터가 더이상 사용되지 않을 것임을 암시
-    return {"q": q}
+def read_items(
+    string_query: str = Query(
+        default="default value",
+        min_length=2,
+        max_length=5,
+        regex="^[a-zA-Z]+$",
+        title="String Query",
+        example="abc",
+    ),
+    number_query: float = Query(
+        default=1.0,
+        ge=0.5,
+        le=10.5,
+        title="Number Query",
+        example=5.5
+    )
+):
+    return {
+        "string_query_handled": string_query,
+        "number_query_handled": number_query
+    }
