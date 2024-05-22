fc8a4c75cb6e78f7d574034106f131166f503795
hagd <hagd0520@gmail.com>
2024-05-22 17:19:30 +0900

    8-2-2 Body()

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
-from fastapi import FastAPI, Query
+from fastapi import Body, FastAPI
 
 
 app = FastAPI()
 
 
-@app.get("/info")
-def read_info(info: str = Query(None, description="정보를 입력해 주세요.")):
-    return {"info": info}
+@app.post("/items")
+def create_item(item: dict = Body(...)):
+    return {"item": item}
 
 
-@app.get("/items")
-def read_items(
-    string_query: str = Query(
-        default="default value",
-        min_length=2,
-        max_length=5,
-        regex="^[a-zA-Z]+$",
-        title="String Query",
-        example="abc",
+# @app.post("/advanced_items")
+# def create_advanced_item(item: dict = Body(
+#     default=None,
+#     example={"key": "value"},
+#     alias="item_alias",
+#     title="Sample Item",
+#     description="This is a sample item",
+#     deprecated=False
+# )):
+#     return {"item": item}
+
+
+@app.post("/advanced_items")
+def create_advanced_item(
+    item: dict = Body(
+        default=None,
+        example={"key": "value"},
+        alias="item_alias",
+        title="Sample Item",
+        description="This is a sample item",
+        deprecated=False
     ),
-    number_query: float = Query(
-        default=1.0,
-        ge=0.5,
-        le=10.5,
-        title="Number Query",
-        example=5.5
+    additional_info: dict = Body(
+        default=None,
+        example={"info_key": "info_value"},
+        title="Additional Info",
+        description="This is some additional information about the item",
+        deprecated=False
     )
 ):
     return {
-        "string_query_handled": string_query,
-        "number_query_handled": number_query
+        "item": item,
+        "additional_info": additional_info
     }
