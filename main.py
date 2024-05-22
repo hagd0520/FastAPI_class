7aa9b1b8fce946a54c8dbb0272b5f06727baca82
hagd <hagd0520@gmail.com>
2024-05-22 15:40:25 +0900

    7-1 response_model

--- main.py
+++ main.py
@@ -5,13 +5,48 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-
 class Item(BaseModel):
     name: str
-    tags: List[str]
-    variant: Union[int, str]
+    description: str = None
+    price: float
+    
+
+def get_item_from_db(id):
+    # 매우 간단한 아이템 반환
+    return {
+        "name": "Simple Item",
+        "description": "A simple item description",
+        "price": 50.0,
+        "dis_pirce": 45.0
+    }
     
     
-@app.post("/items")
-def create_item(item: Item):
-    return {"item": item.model_dump()}
+@app.get("/items/{item_id}", response_model=Item)
+def read_item(item_id: int):
+    item = get_item_from_db(item_id)
+    return item
+
+
+class Cat(BaseModel):
+    name: str
+    
+    
+class Dog(BaseModel):
+    name: str
+    
+    
+@app.get("/animal", response_model=Cat)
+async def get_animal(animal: str):
+    if animal == "cat":
+        return Dog(name="Whiskers")
+    else:
+        return Dog(name="Fido")
+    
+    
+class Item2(BaseModel):
+    name: str
+    
+    
+@app.get("/items", response_model=List[Item2])
+async def get_items():
+    return [{"name": "Item 1"}, {"name": "Item 2"}]
